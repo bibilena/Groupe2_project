1a5413bd3a1e82b5086a9df734cc65ad0adfdc17
Olivier Hayot <olivier.hayot.dev@gmail.com>
2021-11-24 08:44:02 +0100

    Commit initial, création de la structure du projet

---
+++ src/test/java/be/heh/ec/ecproject/EcProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package be.heh.ec.ecproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
