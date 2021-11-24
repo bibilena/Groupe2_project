26dbe47b5aeb8aed7914eb525131ccc09c007a3c
Olivier Hayot <olivier.hayot.dev@gmail.com>
2021-11-24 16:03:59 +0100

    Création de fichier system.properties

--- src/main/java/be/heh/ec/ecproject/product/adapters/in/web/ProductController.java
+++ src/main/java/be/heh/ec/ecproject/product/adapters/in/web/ProductController.java
@@ -1,10 +1,7 @@
 package be.heh.ec.ecproject.product.adapters.in.web;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import be.heh.ec.ecproject.product.application.in.AllProductUseCase;
 import java.util.Map;
