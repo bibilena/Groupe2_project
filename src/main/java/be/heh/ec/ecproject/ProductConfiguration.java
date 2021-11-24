aad1e228ec714b8af108e662faec1d4b85d66d0e
Olivier Hayot <olivier.hayot.dev@gmail.com>
2021-11-24 16:20:13 +0100

    Liaision à la base de donnée de Heroku

--- src/main/java/be/heh/ec/ecproject/ProductConfiguration.java
+++ src/main/java/be/heh/ec/ecproject/ProductConfiguration.java
@@ -6,10 +6,12 @@ import be.heh.ec.ecproject.product.application.in.AllProductUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @EnableJpaRepositories
+@Profile("prod")
 public class ProductConfiguration {
 
     @Autowired
